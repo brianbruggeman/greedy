36c255500adcd7e45c882092933ee2b59aec4d42
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-04-11 11:47:04 -0500

    Fixed merge_row_indices

--- greedy.py
+++ greedy.py
@@ -13,19 +13,19 @@ def capture_row_indices(materials, shape_only=False):
     opposed to actual details of materials.  This should provide a close
     to optimal pairing of indices.
 
-    >>> materials = [2, 0, 0, 1]
-    >>> capture_row_indices(materials, shape_only=True)
-    [(0, 0), (3, 3)]
-
-    >>> materials = [2, 0, 0, 1]
-    >>> capture_row_indices(materials)
-    [(0, 0), (3, 3)]
-
-    >>> materials = [1, 2, 2, 1]
+    >>> materials = [2, 1, 1, 2]
     >>> capture_row_indices(materials, shape_only=True)
     [(0, 3)]
 
-    >>> materials = [1, 2, 2, 1]
+    >>> materials = [2, 1, 1, 2]
+    >>> capture_row_indices(materials)
+    [(0, 0), (1, 2), (3, 3)]
+
+    >>> materials = [2, 0, 0, 1]
+    >>> capture_row_indices(materials, shape_only=True)
+    [(0, 0), (3, 3)]
+
+    >>> materials = [2, 1, 1, 2]
     >>> indices = capture_row_indices(materials)
     >>> print indices
     [(0, 0), (1, 2), (3, 3)]
@@ -54,7 +54,7 @@ def capture_row_indices(materials, shape_only=False):
         last_idx = idx
     if start and last:
         row_data.append((start_idx, last_idx))
-    return row_data
+    return sorted(row_data)
 
 
 def generate_points(row_index, row):
@@ -84,19 +84,28 @@ def merge_row_indices(rows):
         else:
             filtered = filter(set(row).__contains__, last_row)
             if filtered:
-                for grid_index, gpairs in enumerate(grid):
-                    gstart, gend = gpairs
-                    for rstart, rend in row:
-                        start = (row_index, rstart)
-                        end = (row_index, rend)
-                        if gstart[-1] == start[-1]:
-                            if gend[0] == end[0] - 1:
-                                grid_entry = grid[grid_index]
-                                grid_entry[-1] = end
+                for rindex, rdata in enumerate(row):
+                    rmatched = False
+                    rstart, rend = rdata
+                    start = (row_index, rstart)
+                    end = (row_index, rend)
+                    # match up with grid entries
+                    for grid_index, gpairs in enumerate(grid):
+                        gstart, gend = gpairs
+                        starts_match = gstart[-1] == start[-1]
+                        ends_match = gend[0] == end[0] - 1
+                        if starts_match and ends_match:
+                            grid_entry = grid[grid_index]
+                            grid_entry[-1] = end
+                            rmatched = True
+                            break
+                    # add if not matched
+                    if not rmatched:
+                        grid.extend(list(generate_points(row_index, [rdata])))
             else:
                 grid.extend(list(generate_points(row_index, row)))
         last_row = row
-    print pformat(grid)
+    return sorted(grid)
 
 
 def greedy_index(materials, stride=None, dim=None, shape_only=False):
@@ -110,13 +119,30 @@ def greedy_index(materials, stride=None, dim=None, shape_only=False):
     shape_only will ignore material id and match based on a binary data
     or no data criteria.
 
+    >>> materials = [
+    ...     [2, 1, 1, 2],
+    ...     [2, 0, 0, 2],
+    ...     [2, 0, 0, 2],
+    ...     [2, 1, 1, 2],
+    ... ]
+    >>> greedy_index(materials, shape_only=True)
+    [[(0, 0), (0, 3)], [(1, 0), (2, 0)], [(1, 3), (2, 3)], [(3, 0), (3, 3)]]
+
+    >>> materials = [
+    ...     [2, 1, 1, 2],
+    ...     [2, 0, 0, 2],
+    ...     [2, 0, 0, 2],
+    ...     [2, 1, 1, 2],
+    ... ]
+    >>> greedy_index(materials)
+    [[(0, 0), (3, 0)], [(0, 1), (0, 2)], [(0, 3), (3, 3)], [(3, 1), (3, 2)]]
     """
+
     rows = []
     for row in materials:
-        indices = capture_row_indices(materials, shape_only)
+        indices = capture_row_indices(row, shape_only)
         rows.append(indices)
-    grid = merge_row_indices(rows)
-    return grid
+    return merge_row_indices(rows)
 
 
 if __name__ == "__main__":
